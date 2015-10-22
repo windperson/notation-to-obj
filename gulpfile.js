819c2b1dda6b22a39f6249772b9bfd975eca84f2
windperson <windperson@gmail.com>
2015-10-22 14:46:21 +0800

    can run unit test in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var ts = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 var vinylPaths = require('vinyl-paths');
+var path = require('path');
 var merge = require('merge-stream');
 var runseq = require('run-sequence');
 var mocha = require('gulp-mocha');
@@ -11,17 +12,18 @@ var path_config = {
 	src: {
 		ts_config: 'src/lib/tsconfig.json'
 	},
-	test: {
-		ts_config: 'src/test/mocha/tsconfig.json',
-		dest: 'test',
-		clean: ['test/**/*','src/test/**/*.js','src/test/**/*.js.map','src/lib/**/*.js','src/lib/**/*.js.map']
-	},
 	out: {
 		dest: 'release',
 		def_dest: 'release/definitions',
 		clean: ['release/**/*']
+	},
+	test: {
+		ts_config: 'src/test/mocha/tsconfig.json',
+		dest: 'test',
+		clean: ['test/**/*']
 	}
 };
+
 
 function buildTypeScript(tsProject) {
 	return tsProject.src({ base: "" })
@@ -29,7 +31,7 @@ function buildTypeScript(tsProject) {
 		.pipe(ts(tsProject));
 }
 
-function addSourceMap(gulpStream){
+function addSourceMap(gulpStream) {
 	return gulpStream.pipe(
         sourcemaps.write('.', {
             sourceRoot: function (file) { return ''; },
@@ -43,43 +45,44 @@ gulp.task('compile:src', function () {
 	return merge(
 		addSourceMap(tsResult.js).pipe(gulp.dest(path_config.out.dest)),
 		tsResult.dts.pipe(gulp.dest(path_config.out.def_dest))
-	);
-	
+		);
 });
 
-gulp.task('copy:build',function(){
-	return gulp.src(path_config.out.clean).pipe(gulp.dest(path_config.test.dest))
+gulp.task('copy:build', function () {
+	var buildDest = path.join(path_config.test.dest, './lib');
+	return gulp.src(path_config.out.clean).pipe(gulp.dest(buildDest))
 })
 
 gulp.task('compile:test', function () {
 	var tsProject = ts.createProject(path_config.test.ts_config);
 	var tsResult = buildTypeScript(tsProject);
+	var testDest = path.join(path_config.test.dest, './test/mocha');
 	return addSourceMap(tsResult.js)
-        .pipe(gulp.dest(path_config.test.dest));
+        .pipe(gulp.dest(testDest));
 });
 
 gulp.task('clean:src', function () {
 	return gulp.src(path_config.out.clean).pipe(vinylPaths(del));
 });
 
-gulp.task('clean:test', function(){
+gulp.task('clean:test', function () {
 	return gulp.src(path_config.test.clean).pipe(vinylPaths(del));
 });
 
-gulp.task('build',function(callback){
-	return runseq('clean:src','compile:src',callback);
+gulp.task('build', function (callback) {
+	return runseq('clean:src', 'compile:src', callback);
 });
 
-gulp.task('build:test',function(callback){
-	return runseq('compile:src','compile:test',callback);
+gulp.task('build:test', function (callback) {
+	return runseq(['compile:src', 'compile:test'], 'copy:build', callback);
 });
 
 gulp.task('test:mocha', function () {
-    return gulp.src([path_config.test.dest + '/**/*.js'], {read: false})
-        // gulp-mocha needs filepaths so you can't have any plugins before it 
+    return gulp.src([path_config.test.dest + '/test/**/*.js'], { read: false })
+	// gulp-mocha needs filepaths so you can't have any plugins before it 
         .pipe(mocha());
 });
 
-gulp.task('test',function(callback){
-	return runseq(['build:test','test:mocha'])
+gulp.task('test', function (callback) {
+	return runseq('build:test', 'test:mocha')
 });
